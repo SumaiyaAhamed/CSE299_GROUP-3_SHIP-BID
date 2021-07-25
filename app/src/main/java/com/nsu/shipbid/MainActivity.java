25b36e839e8bb57ea0bc10a552b6c1f82817d073
SumaiyaAhamed <sumaiya.ahamed@northsouth.edu>
2021-07-26 02:43:24 +0600

    initial commit

---
+++ app/src/main/java/com/nsu/shipbid/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nsu.shipbid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
